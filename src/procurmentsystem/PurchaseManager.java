e9705806660467455270c913e706cf46ec5a67ec
aspyring <74718196+aspyring@users.noreply.github.com>
2024-12-18 09:30:39 +0800

    -Revamped requisition class to match UML diagram
    -Skeleton saleManager class created
    
    -added APPROVED to Status enum
    
    -requisition.csv updated, removed dateCreated.

--- src/procurmentsystem/PurchaseManager.java
+++ src/procurmentsystem/PurchaseManager.java
@@ -8,8 +8,5 @@ public class PurchaseManager {
     //show user purchase manager, name and uuid
     //view the purchase order list
     //create a purchase order
-    public void generatePurchaseOrder(String orderID, String requisitionID, String itemID, int price){
-        PurchaseOrder po = new PurchaseOrder(orderID, requisitionID, itemID, price);
-        purchaseOrders.add(po); //TBA
-    }
+
 }
